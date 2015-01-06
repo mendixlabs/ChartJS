d6612917e90c72dc9d52e35a2628cef25b6dc9b0
Robert van 't Hof <robert.van.thof@mendix.com>
2015-01-06 16:51:26 +0100

    Added widget source (still under construction)

---
+++ README.md
@@ -0,0 +1,85 @@
+# App Store Widget Boilerplate
+
+This boilerplate gives you all you need to start a new custom widget for Mendix 5.
+
+The boilerplate contains:
+
+- Directory structure.
+- Readme.md file.
+- License.
+- Javascript source.
+- XSD for package.xml, to configure properties of the widget, visible inside the Mendix business modeler.
+
+## Contributing
+
+For more information on contributing to this repository visit [Contributing to a GitHub repository](https://world.mendix.com/display/howto50/Contributing+to+a+GitHub+repository)!
+
+## Typical usage scenario
+
+Use this template to start building a widget for Mendix 5.
+Alter this README.md file and describe what your widget does.
+ 
+## Description
+
+The javascript inside the widget has examples of:
+
+- Using CSS within a Widget.
+- Using templating.
+- Loading external library's.
+- DOM manipulation.
+- Event attaching.
+- Loading data.
+- Executing microflow and sending data.
+- Working with the context object (The object that is send by a contextview , for instance a dataview).
+
+### Dojo AMD module list
+
+The javascript contains a really extensive list of modules that may be used to build the widget. It is best to reduce this list to what is actually used. Plugins like jsLint for Notepad++ will show unused objects. **Be sure to keep the module name array and the parameter list of the anonymous function below the module list in sync!**
+
+The following modules are necessary for all widgets:
+
+- dojo/_base/declare
+- mxui/widget/_WidgetBase
+- dijit/_Widget
+
+If your widget does not use an HTML template:
+
+- Remove dijit/_TemplatedMixin from the module list
+- Remove _Templated from the parameter list of the anonymous function below the module list
+- Remove _Templated from the parameter list of the declare call
+- Remove the templates folder
+
+If your widget does not need jQuery:
+
+- Remove WidgetName/widget/lib/jquery from the module list
+- Remove _jQuery from the parameter list of the anonymous function below the module list
+- Remove _jQuery from the parameter list of the declare call
+- Remove jquery.js from src\WidgetName\widget\lib\ Or remove the lib folder if you don't include external libraries in the widget.
+
+## Migrating a widget to Dojo AMD
+
+A widget that uses Dojo AMD may not refer to functions like *dojo.query* etc. All necessary modules must be declared on the module list at the top of the source.
+
+Replacing all 'old' Dojo calls in an existing source can be a bit of a pain.
+
+Here is a list of commonly used functions and their AMD counterpart:
+
+Old | New
+---------- |---------- 
+mxui.dom              | domMx
+dojo.byId             | dom.byId
+dojo.query            | domQuery
+dojo.forEach          | dojoArray.forEach
+dojo.hitch            | lang.hitch
+dojo.addClass         | domClass.add
+dojo.removeClass      | domClass.remove
+dojo.hasClass         | domClass.contains
+dojo.replaceClass     | domClass.replace
+dojo.empty            | domConstruct.empty
+dojo.place            | domConstruct.place 
+dojo.on               | on
+dojo.window           | win
+  
+Do not use domQuery.query! Just domQuery
+
+The referenced modules are in the module list of the boilerplate javascript
---
+++ src/ChartJS/widgets/Bar/widget/Bar.js
@@ -0,0 +1,165 @@
+/*global mx, mendix, require, console, define, module, logger */
+(function () {
+	'use strict';
+
+	// Required module list. Remove unnecessary modules, you can always get them back from the boilerplate.
+	require([
+
+		'dojo/_base/declare', 'mxui/widget/_WidgetBase', 'dijit/_Widget', 'dijit/_TemplatedMixin',
+		'mxui/dom', 'dojo/dom', 'dojo/query', 'dojo/dom-prop', 'dojo/dom-geometry', 'dojo/dom-class', 'dojo/dom-style', 'dojo/dom-construct', 'dojo/_base/array', 'dojo/window', 'dojo/on', 'dojo/_base/lang', 'dojo/text',
+		'ChartJS/lib/charts'
+
+	], function (declare, _WidgetBase, _Widget, _Templated, domMx, dom, domQuery, domProp, domGeom, domClass, domStyle, domConstruct, dojoArray, win, on, lang, text, _charts) {
+
+		// Declare widget.
+		return declare('ChartJS.widgets.Bar.widget.Bar', [ _WidgetBase, _Widget, _Templated, _charts ], {
+
+			// Template path
+			templatePath: require.toUrl('ChartJS/widgets/Bar/widget/templates/bar.html'),
+
+			_chartJS : null,
+			_chart : null,
+			_ctx : null,
+			_dataset : null,
+
+			startup: function () {
+				this._chartJS = _charts().chartssrc();
+				this._chartJS.defaults.global.responsive = true;
+
+				domStyle.set(this.domNode, {
+					padding: 0,
+					width : '100%',
+					maxWidth : '100%',
+					maxHeight : '100%',
+					overflow : 'hidden'
+				});
+
+				this._ctx = this.canvasNode.getContext("2d");
+				this._dataset = this.datasetentity.split("/")[0];
+				this._datapoint = this.datapointentity.split("/")[0];
+			},
+
+			update : function (obj, callback) {
+				this._executeMicroflow(lang.hitch(this, function (objs) {
+					var obj = objs[0],
+						data = {
+							datasets : []
+						},
+						xlabels = [],
+						xlabelsSet = false;
+
+					mx.data.get({
+						guids : obj.get(this._dataset),
+						callback : lang.hitch(this, function (finalCallback, datasets) {
+
+							var len = datasets.length;
+
+							for(var j=0;j < datasets.length; j++) {
+								var dataset = datasets[j];
+
+								mx.data.get({
+									guids : dataset.get(this._datapoint),
+									callback : lang.hitch(this, function ( dataset, len, j, finalCallback, datapoints) {
+										var points = [];
+
+										var datapoints = this._sortDatapoints(datapoints);
+
+										for(var i=0;i < datapoints.length; i++) {
+											if (!xlabelsSet)
+												xlabels.push(datapoints[i].get(this.seriesxlabel));
+
+											points.push(datapoints[i].get(this.seriesylabel));
+										}
+
+										if (!xlabelsSet)
+											xlabelsSet = true;
+										var color = dataset.get(this.seriescolor);
+
+										data.datasets.push({
+											label : dataset.get(this.datasetlabel),
+											fillColor: this._hexToRgb(color, "0.5"),
+											strokeColor: this._hexToRgb(color, "0.8"),
+											highlightFill: this._hexToRgb(color, "0.75"),
+											highlightStroke: this._hexToRgb(color, "1"),
+											data : points
+										});
+
+										if (len-1 === j){
+											finalCallback();
+										}
+
+									}, dataset, len, j, finalCallback)
+								});
+							}
+						}, lang.hitch(this, function () {
+							data.labels = xlabels;
+							
+							var myNewChart = null;
+							if (this.chartType === "Bar")
+								myNewChart = new this._chartJS(this._ctx).Bar(data);
+							else if (this.chartType === "Radar")
+								myNewChart = new this._chartJS(this._ctx).Radar(data);
+							else
+								myNewChart = new this._chartJS(this._ctx).Line(data);
+						}))
+					});
+				}));
+
+				callback && callback();
+			},
+
+			_sortDatapoints : function (points) {
+				return points.sort(lang.hitch(this, function (a,b) {
+					var aa = a.get(this.sortingxvalue);
+					var bb = b.get(this.sortingxvalue);
+					if (aa > bb) {
+						return 1;
+					}
+					if (aa < bb) {
+						return -1;
+					}
+					// a must be equal to b
+					return 0;
+				}));
+			},
+
+			_hexToRgb : function (hex, alpha) {
+				// From Stackoverflow here: http://stackoverflow.com/questions/5623838/rgb-to-hex-and-hex-to-rgb
+				// Expand shorthand form (e.g. "03F") to full form (e.g. "0033FF")
+				var shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
+				hex = hex.replace(shorthandRegex, function(m, r, g, b) {
+					return r + r + g + g + b + b;
+				});
+
+				var regex = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+				if (regex) {
+					var result = {
+						r: parseInt(regex[1], 16),
+						g: parseInt(regex[2], 16),
+						b: parseInt(regex[3], 16)
+					};
+					return "rgba("+result.r+","+result.g+","+result.b+","+alpha+")";
+				}
+				return "rgba(220,220,220,"+alpha+")";
+			},
+
+			_executeMicroflow : function (callback) {
+				mx.data.action({
+					params: {
+						applyto: 'selection',
+						actionname: this.datasourcemf,
+						guids: []
+					},
+					callback: lang.hitch(this, callback),
+					error: function (error) {
+						console.log(error.description);
+					}
+				}, this);
+			}
+
+		});
+	});
+
+}());
+
+
